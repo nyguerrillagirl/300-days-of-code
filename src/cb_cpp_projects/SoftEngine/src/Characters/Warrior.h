cf748bbf42ebc33729af9e05a68d1ce56f2d1729
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-05-22 07:41:23 -0400

    Day #237 - Working on JGE (new project) + CRVG + Joined Engineering Book Club

--- src/cb_cpp_projects/SoftEngine/src/Characters/Warrior.h
+++ src/cb_cpp_projects/SoftEngine/src/Characters/Warrior.h
@@ -4,6 +4,11 @@
 #include "Character.h"
 #include "Animation.h"
 #include "RigidBody.h"
+#include "Collider.h"
+#include "Vector2D.h"
+
+#define JUMP_TIME 15.0f
+#define JUMP_FORCE 10.0f
 
 class Warrior : public Character
 {
@@ -16,8 +21,17 @@ class Warrior : public Character
 
 
     private:
+        bool m_IsJumping;   // indicates the player is jumping
+        bool m_IsGrounded;  // indicates the player is grounded (so player can jump)
+
+        float m_JumpTime;   // ?
+        float m_JumpForce;  // the force the player uses to jump up
+
+        Collider* m_Collider;   // defines the collision box around the player
         Animation* m_Animation;
         RigidBody* m_RigidBody;
-};
 
+        // if player collides with something this is the position to restore back to
+        Vector2D m_LastSafePosition;
+};
 #endif // WARRIOR_H
