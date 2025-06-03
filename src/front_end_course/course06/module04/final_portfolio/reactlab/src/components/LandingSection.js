4e3d6eec6b6d98ab113ba662aceb2d5810f14429
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-03 07:02:30 -0400

    Day #249 - Data Camp + Coursera courses + completed reading a book

--- src/front_end_course/course06/module04/final_portfolio/reactlab/src/components/LandingSection.js
+++ src/front_end_course/course06/module04/final_portfolio/reactlab/src/components/LandingSection.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack, Text } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Guerrilla Coder!";
+const bio1 = "An experienced Java backend developer";
+const bio2 = "Learning React to build modern web applications";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +15,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4} align="center">
+      <Avatar size="xl" name="Lorraine Figueroa" src="/avatar.jpg"/>
+      <Text>{greeting}</Text>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+    </VStack>
   </FullScreenSection>
 );
 
