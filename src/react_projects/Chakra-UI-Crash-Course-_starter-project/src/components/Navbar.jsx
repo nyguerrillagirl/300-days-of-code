e49a478b14350862b6c4333113ad97306a9a0d97
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-03 12:46:57 -0400

    Day #250 - Chakra lesson #7 - Card Component

--- src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/components/Navbar.jsx
+++ src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Flex, Box, Heading, Text, Button, Spacer, HStack } from "@chakra-ui/rea
 export default function Navbar() {
 
     return (
-        <Flex as="nav" p="10px" alignItems="center">
+        <Flex as="nav" p="10px" mb="40px" alignItems="center">
             <Heading as="h1">Dojo Tasks</Heading>
             <Spacer />
             <HStack spacing="20px">
