516857bdbd3cdeb16f67c1ff76f29aa2b2ead342
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-04 08:52:48 -0400

    Day #251 - Chakra UI Crash Course #9 - Form Fields

--- src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/App.jsx
+++ src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/App.jsx
@@ -3,20 +3,22 @@ import {
   createRoutesFromElements, 
   Route, 
   RouterProvider 
-} from 'react-router-dom'
+} from 'react-router-dom';
 
 // layouts and pages
-import RootLayout from './layouts/RootLayout'
-import Dashboard, { tasksLoader } from './pages/Dashboard'
-import Create from './pages/Create'
-import Profile from './pages/Profile'
+import RootLayout from './layouts/RootLayout';
+import Dashboard, { tasksLoader } from './pages/Dashboard';
+import Create from './pages/Create';
+import Profile from './pages/Profile';
+import { createAction } from './pages/Create';
+
 
 // router and routes
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Dashboard />} loader={tasksLoader}/>
-      <Route path="create" element={<Create />} />
+      <Route path="create" element={<Create />} action={createAction} />
       <Route path="profile" element={<Profile />} />
     </Route>
   )
