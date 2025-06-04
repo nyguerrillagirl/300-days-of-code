db7b5efc3ff6217c575032263a4ce4c004570b30
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-04 09:28:19 -0400

    Day #251 - Chakra UI Crash Course #12 - Customizing the Theme

--- src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/main.jsx
+++ src/react_projects/Chakra-UI-Crash-Course-_starter-project/src/main.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+// extend the theme
+const colors = {
+  brand: {
+    500: '#4287f5',
+    600: '#337df2',
+    700: '#2977f2',
+    800: '#146af5',
+    900: '#024fc9'
+  }
+}
+
+const fonts = {
+  heading: 'Courier New',
+  body: 'Tahoma'
+}
+const theme = extendTheme({colors, fonts});
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <React.StrictMode>
-      <ChakraProvider>
+     <React.StrictMode>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </React.StrictMode>
