4a85b5122513b57998d7c92f35424fe5312cc5f8
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2025-06-13 07:36:04 -0400

    Day #259 - Coursera courses + Reading

---
+++ src/react_projects/simple-app/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+const ToDo = props => (
+  <tr>
+    <td>
+      <label>{props.id}</label>
+    </td>
+    <td>
+      <input />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
+);
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 'todo1',
+      createdAt: '18:00',
+    }, 
+    {
+      id: 'todo2',
+      createdAt: '20:30',
+    }
+  ]);
+
+  const reverseOrder = () => {
+    // Reverse is a mutative operation, so we need to create a new array first.
+    setTodos([...todos].reverse());
+  };
+
+  return (
+    <div>
+      <button onClick={reverseOrder}>Reverse</button>
+      {todos.map((todo, index) => (
+        <ToDo key={index} id={todo.id} createdAt={todo.createdAt} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
